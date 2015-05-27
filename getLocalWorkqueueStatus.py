bb9a7cf46192055eec182e0dc6b8946de98d90f8
amaltaro <alan.malta@cern.ch>
2015-05-27 14:43:57 +0200

    Adding docs overview by status and workflow

--- getLocalWorkqueueStatus.py
+++ getLocalWorkqueueStatus.py
@@ -13,7 +13,7 @@ Created on Apr 27, 2015.
 """
 
 import sys, os
-
+from pprint import pprint
 from WMCore.Configuration import loadConfigurationFile
 from WMCore.Services.RequestDB.RequestDBReader import RequestDBReader
 from WMComponent.JobCreator.JobCreatorPoller     import retrieveWMSpec
@@ -41,12 +41,11 @@ def main():
     localWQBackend = WorkQueueBackend(config.WorkQueueManager.couchurl, db_name = "workqueue_inbox")
     localWQInboxDB = Database('workqueue', config.WorkQueueManager.couchurl)
 
-    elemKeys = ['Jobs','RequestName', 'Priority', 'SiteWhitelist',
-                'Status', 'SubscriptionId', 'TaskName']
+    elemKeys = ['Jobs','RequestName', 'Priority', 'SiteWhitelist', 'Status', 'TaskName', 'ParentQueueId']
 
     # check local queue
     wqDocIDs = localWQBackend.getElements()
-    print "Found %d local workqueue_inbox docs." % len(wqDocIDs)
+    print "Found %d local workqueue_inbox docs.\n" % len(wqDocIDs)
 
     # print the whole stuff
     filteredElemts = []
@@ -56,8 +55,31 @@ def main():
         for k in elemKeys:
             temp[k] = ele[k]
         filteredElemts.append(temp)
-        print temp
+#        print temp
 
+    # Getting total number of jobs per workqueue docs status
+    STATES = ('Acquired', 'Running', 'Done', 'Failed', 'CancelRequested', 'Canceled')
+    wqStatus = {}
+    for i in filteredElemts:
+        if i['Status'] not in wqStatus:
+            wqStatus[i['Status']] = {'Jobs': i['Jobs'], 'ElemCounter': 1}
+        else:
+            wqStatus[i['Status']]['Jobs'] += i['Jobs']
+            wqStatus[i['Status']]['ElemCounter'] += 1
+    print "Overview of workqueue_inbox elements"
+    pprint(wqStatus)
+
+
+    # ordering by workflow name
+    orderedElemts = {}
+    for i in filteredElemts:
+        if i['RequestName'] not in orderedElemts:
+            orderedElemts[i['RequestName']] = [i]
+        else:
+            orderedElemts[i['RequestName']].append(i)
+        del orderedElemts[i['RequestName']][-1]['RequestName']
+    print "\nOrdering workqueue elements by workflow"
+    pprint(orderedElemts)
 #    if localWQInboxDB.documentExists(wf):
 #        print "Found local workqueue doc for %s, status %s" % (wf, tempWfs[wf]['RequestStatus'])
 
