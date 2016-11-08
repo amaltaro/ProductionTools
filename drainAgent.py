96465f3921a94cfc7b481772b3cb452ac9ee8616
amaltaro <alan.malta@cern.ch>
2016-11-08 22:39:15 +0100

    Added condor and some wmbs checks

--- drainAgent.py
+++ drainAgent.py
@@ -9,8 +9,33 @@ redeployed) and:
  """
 import sys
 import os
-import httplib
-import json
+import threading
+import logging
+from pprint import pprint
+
+try:
+    import htcondor as condor
+    from WMCore.WMInit import connectToDB
+    from WMCore.Database.DBFormatter import DBFormatter
+    from WMCore.Configuration import loadConfigurationFile
+    from WMCore.Services.RequestDB.RequestDBReader import RequestDBReader
+    #from WMCore.DAOFactory import DAOFactory
+except ImportError:
+    print "You do not have a proper environment, please source the following:"
+    print "source /data/srv/wmagent/current/apps/wmagent/etc/profile.d/init.sh"
+    sys.exit(1)
+
+
+
+jobCountByState = """
+    select wmbs_job_state.name, count(*) AS count
+    from wmbs_job
+    join wmbs_job_state on (wmbs_job.state = wmbs_job_state.id)
+    group by wmbs_job.state, wmbs_job_state.name"""
+
+knownWorkflows = "SELECT DISTINCT name from wmbs_workflow"
+
+workflowsNotInjected = "select distinct name from wmbs_workflow where injected = 0"
 
 
 def lfn2dset(lfns):
@@ -32,27 +57,6 @@ def lfn2dset(lfns):
     return listDsets
 
 
-def getRequestDict(wfs):
-    """ Retrieve the request workload schema from reqmgr2 """
-    if isinstance(wfs, basestring):
-        wfs = [wfs]
-
-    url = 'cmsweb.cern.ch'
-    head = {"Content-type": "application/json", "Accept": "application/json"}
-
-    schema = {}
-    conn = httplib.HTTPSConnection(url, cert_file=os.getenv('X509_USER_PROXY'), key_file=os.getenv('X509_USER_PROXY'))
-    for wf in set(wfs):
-        urn = "/reqmgr2/data/request/%s" % wf
-        conn.request("GET", urn, headers=head)
-        resp = conn.getresponse()
-        data = resp.read()
-        request = json.loads(data)["result"][0][wf]
-        schema[wf] = filterKeys(request)
-
-    return schema
-
-
 def filterKeys(schema):
     """ Saves only input and output dataset info """
     newSchema = {}
@@ -71,30 +75,86 @@ def filterKeys(schema):
     return newSchema
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("Usage: python drainAgent.py LIST_OF_WFS LIST_OF_LFNs")
-        sys.exit(0)
+def getWorkflowStatus(config, listOfWfs):
+    """
+    Given a list of workflows, return their name and RequestStatus
+    """
+    if isinstance(listOfWfs, basestring):
+        listOfWfs = [listOfWfs]
 
-    wfsFile = sys.argv[1]
-    with open(wfsFile) as f:
-        wfsList = [line.rstrip('\n') for line in f]
+    wfDBReader = RequestDBReader(config.AnalyticsDataCollector.centralRequestDBURL,
+                                 couchapp = config.AnalyticsDataCollector.RequestCouchApp)
+    tempWfs = wfDBReader.getRequestByNames(listOfWfs, True)
 
-    lfnsFile = sys.argv[2]
-    with open(lfnsFile) as f:
-        lfnsList = [line.rstrip('\n') for line in f]
+    for wf in listOfWfs:
+        print("%-125s\t%s" % (wf, tempWfs[wf]['RequestStatus']))
 
-    dsetsList = lfn2dset(lfnsList)
-    requestsDict = getRequestDict(wfsList)
+    return
 
-    # now find these bastards
-    for dset in dsetsList:
-        print("%-150s is ..." % dset),
-        for wf, data in requestsDict.iteritems():
-            if dset == data['InputDataset']:
-                print(" INPUT for %s which is in %s status" % (wf, data['RequestStatus'])),
-            if dset in data['OutputDatasets']:
-                print(" OUTPUT for %s which is in %s status" % (wf, data['RequestStatus'])),
-        print("")
+def getCondorJobs():
+    """
+    Retrieve jobs from condor.
+    :return: amount of jobs ordered by JobStatus and workflow
+    """
+    jobDict = {}
+    schedd = condor.Schedd()
+    jobs = schedd.xquery('true', ['WMAgent_RequestName', 'JobStatus'])
+    for job in jobs:
+        jobStatus = job['JobStatus']
+        jobDict.setdefault(jobStatus, {})
+        jobDict[jobStatus].setdefault(job['WMAgent_RequestName'], 0)
+        jobDict[jobStatus][job['WMAgent_RequestName']] += 1
+    return jobDict
+
+
+def getWMBSInfo(config):
+    """
+    blah
+    :return:
+    """
+    connectToDB()
+    myThread = threading.currentThread()
+    formatter = DBFormatter(logging, myThread.dbi)
+
+    jobsByState = formatter.formatDict(myThread.dbi.processData(jobCountByState))
+    print("\n*** Amount of wmbs jobs ordered by status:\n%s" % jobsByState)
+
+    workflows = formatter.formatDict(myThread.dbi.processData(knownWorkflows))
+    workflows = [wf['name'] for wf in workflows]
+    print("\n*** Distinct workflows known by this agent:")
+    getWorkflowStatus(config, workflows)
+
+    wfsNotInjected = formatter.format(myThread.dbi.processData(workflowsNotInjected))
+    wfsNotInjected = [wf['name'] for wf in wfsNotInjected]
+    print("\n*** Workflows not fully injected:\n")
+    getWorkflowStatus(config, wfsNotInjected)
+
+
+def main():
+    """
+    Retrieve the following information from the agent:
+      1. number of jobs in condor
+      2. list of distinct workflows in wmbs_workflow (and their status in reqmgr2)
+      3. amount of wmbs jobs in each status
+      4. list of workflows not fully injected
+      5. list of subscriptions not finished
+      6. amount of files available in wmbs
+      7. amount of files acquired in wmbs
+      8. list of blocks not closed in phedex/dbs
+      9. list of files not uploaded to dbs
+      10. list of files not injected into phedex, with parent block
+      11. list of files not injected into phedex, without parent block
+    """
+    os.environ['WMAGENT_CONFIG'] = '/data/srv/wmagent/current/config/wmagent/config.py'
+    config = loadConfigurationFile(os.environ["WMAGENT_CONFIG"])
+
+    print("\n*** Jobs in condor sorted by condor job status and workflow:")
+    pprint(getCondorJobs())
+
+    getWMBSInfo(config)
 
     sys.exit(0)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
