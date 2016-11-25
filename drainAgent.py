187032c237e5801c08c3c074c854492824d499a5
Alan Malta Rodrigues <amaltaro@uscmsmac02.dyndns.cern.ch>
2016-11-25 14:16:25 +0100

    Added the other checks; missing phedex-fix

--- drainAgent.py
+++ drainAgent.py
@@ -7,35 +7,74 @@ redeployed) and:
  by the agent)
  3. print whether it's an input or output dataset of a know workflow
  """
+from __future__ import print_function
+
 import sys
 import os
 import threading
 import logging
 from pprint import pprint
-
 try:
     import htcondor as condor
+    from Utils.IterTools import flattenList
     from WMCore.WMInit import connectToDB
     from WMCore.Database.DBFormatter import DBFormatter
     from WMCore.Configuration import loadConfigurationFile
     from WMCore.Services.RequestDB.RequestDBReader import RequestDBReader
     #from WMCore.DAOFactory import DAOFactory
 except ImportError:
-    print "You do not have a proper environment, please source the following:"
-    print "source /data/srv/wmagent/current/apps/wmagent/etc/profile.d/init.sh"
+    print("You do not have a proper environment, please source the following:")
+    print("source /data/srv/wmagent/current/apps/wmagent/etc/profile.d/init.sh")
     sys.exit(1)
-
 
 
 jobCountByState = """
     select wmbs_job_state.name, count(*) AS count
-    from wmbs_job
-    join wmbs_job_state on (wmbs_job.state = wmbs_job_state.id)
-    group by wmbs_job.state, wmbs_job_state.name"""
+      from wmbs_job
+      join wmbs_job_state on (wmbs_job.state = wmbs_job_state.id)
+      group by wmbs_job.state, wmbs_job_state.name
+    """
 
 knownWorkflows = "SELECT DISTINCT name from wmbs_workflow"
 
 workflowsNotInjected = "select distinct name from wmbs_workflow where injected = 0"
+
+unfinishedSubscriptions = """
+   select distinct wmbs_workflow.name AS wfName
+     FROM wmbs_subscription
+     INNER JOIN wmbs_fileset ON wmbs_subscription.fileset = wmbs_fileset.id
+     INNER JOIN wmbs_workflow ON wmbs_workflow.id = wmbs_subscription.workflow
+     where wmbs_subscription.finished = 0 ORDER BY wmbs_workflow.name
+   """
+
+filesAvailWMBS = "select subscription,count(*) from wmbs_sub_files_available group by subscription"
+
+filesAcqWMBS = "select subscription,count(*) from wmbs_sub_files_acquired group by subscription"
+
+blocksOpenDBS = "SELECT * FROM dbsbuffer_block WHERE status!='Closed'"
+
+filesNotInDBS = "SELECT * from dbsbuffer_file where status = 'NOTUPLOADED'"
+
+filesNotInPhedex = """
+    SELECT lfn FROM dbsbuffer_file
+      WHERE in_phedex=0
+      AND block_id IS NOT NULL
+      AND lfn NOT LIKE '%%unmerged%%'
+      AND lfn NOT LIKE 'MCFakeFile%%'
+      AND lfn NOT LIKE '%%BACKFILL%%'
+      AND lfn NOT LIKE '/store/backfill/%%'
+      AND lfn NOT LIKE '/store/user%%'
+    """
+filesNotInPhedexNull = """
+    SELECT lfn FROM dbsbuffer_file
+      WHERE in_phedex=0
+      AND block_id IS NULL
+      AND lfn NOT LIKE '%%unmerged%%'
+      AND lfn NOT LIKE 'MCFakeFile%%'
+      AND lfn NOT LIKE '%%BACKFILL%%'
+      AND lfn NOT LIKE '/store/backfill/%%'
+      AND lfn NOT LIKE '/store/user%%'
+    """
 
 
 def lfn2dset(lfns):
@@ -83,7 +122,7 @@ def getWorkflowStatus(config, listOfWfs):
         listOfWfs = [listOfWfs]
 
     wfDBReader = RequestDBReader(config.AnalyticsDataCollector.centralRequestDBURL,
-                                 couchapp = config.AnalyticsDataCollector.RequestCouchApp)
+                                 couchapp=config.AnalyticsDataCollector.RequestCouchApp)
     tempWfs = wfDBReader.getRequestByNames(listOfWfs, True)
 
     for wf in listOfWfs:
@@ -117,7 +156,7 @@ def getWMBSInfo(config):
     formatter = DBFormatter(logging, myThread.dbi)
 
     jobsByState = formatter.formatDict(myThread.dbi.processData(jobCountByState))
-    print("\n*** Amount of wmbs jobs ordered by status:\n%s" % jobsByState)
+    print("\n*** Amount of wmbs jobs in each status:\n%s" % jobsByState)
 
     workflows = formatter.formatDict(myThread.dbi.processData(knownWorkflows))
     workflows = [wf['name'] for wf in workflows]
@@ -128,6 +167,31 @@ def getWMBSInfo(config):
     wfsNotInjected = [wf['name'] for wf in wfsNotInjected]
     print("\n*** Workflows not fully injected:\n")
     getWorkflowStatus(config, wfsNotInjected)
+
+    unfinishedSubs = formatter.formatDict(myThread.dbi.processData(unfinishedSubscriptions))
+    print("\n*** Subscriptions not finished:\n%s" % unfinishedSubs)
+
+    filesAvailable = formatter.formatDict(myThread.dbi.processData(filesAvailWMBS))
+    print("\n*** Files still available in WMBS (waiting for job creation):\n%s" % filesAvailable)
+
+    filesAcquired = formatter.formatDict(myThread.dbi.processData(filesAcqWMBS))
+    print("\n*** Files acquired in WMBS (waiting for jobs to finish):\n%s" % filesAcquired)
+
+    blocksopenDBS = formatter.formatDict(myThread.dbi.processData(blocksOpenDBS))
+    print("\n*** Found %d blocks open in DBS." % len(blocksopenDBS), end="")
+    print(" Printing the first 20 blocks only:\n%s" % blocksopenDBS[:20])
+
+    filesnotinDBS = formatter.formatDict(myThread.dbi.processData(filesNotInDBS))
+    print("\n*** Found %d files not uploaded to DBS." % len(filesnotinDBS), end="")
+    print(" Printing the first 20 lfns only:\n%s" % filesnotinDBS[:20])
+
+    filesnotinPhedex = flattenList(formatter.format(myThread.dbi.processData(filesNotInPhedex)))
+    print("\n*** Found %d files not injected in PhEDEx, with valid block id (recoverable)." % len(filesnotinPhedex), end="")
+    print(" Printing the first 20 lfns only:\n%s" % filesnotinPhedex[:20])
+
+    filesnotinPhedexNull = flattenList(formatter.format(myThread.dbi.processData(filesNotInPhedexNull)))
+    print("\n*** Found %d files not injected in PhEDEx, with valid block id (unrecoverable)." % len(filesnotinPhedexNull), end="")
+    print(" Printing the first 20 lfns only:\n%s\n" % filesnotinPhedexNull[:20])
 
 
 def main():
@@ -148,7 +212,7 @@ def main():
     os.environ['WMAGENT_CONFIG'] = '/data/srv/wmagent/current/config/wmagent/config.py'
     config = loadConfigurationFile(os.environ["WMAGENT_CONFIG"])
 
-    print("\n*** Jobs in condor sorted by condor job status and workflow:")
+    print("\n*** Amount of jobs in condor per workflow, sorted by condor job status:")
     pprint(getCondorJobs())
 
     getWMBSInfo(config)
