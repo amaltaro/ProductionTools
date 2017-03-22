ce2ab480c6d49f4564627f45deb6dc663ac5e03c
Alan Malta Rodrigues <amaltaro@uscmsmac02.dyndns.cern.ch>
2017-03-22 14:08:11 +0100

    Do not restart ErrorHandler if the agent is completely down

--- restartComponent.sh
+++ restartComponent.sh
@@ -5,6 +5,12 @@ DATENOW=`date +%s`
 LASTCHANGE=`stat -c %Y /data/srv/wmagent/current/install/wmagent/ErrorHandler/ComponentLog`
 INTERVAL=`expr $DATENOW - $LASTCHANGE`
 if (("$INTERVAL" >= 900)); then
+  OTHERS=`ps aux | grep wmcore | grep -v grep`
+  if [[ -z "$OTHERS" ]]; then
+    echo "Since the agent is not running, don't do anything ..."
+    exit 1
+  fi
+
   . /data/admin/wmagent/env.sh
   /data/srv/wmagent/current/config/wmagent/manage execute-agent wmcoreD --restart --components=ErrorHandler
   echo "cronjob in $HOST" | mail -s "ErrorHandler restarted" alan.malta@cern.ch,sryu@fnal.gov
