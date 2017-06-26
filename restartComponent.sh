53b00a25865cbf6e766811ec5e82214ce33b84df
amaltaro <alan.malta@cern.ch>
2017-06-26 11:10:54 +0200

    Restart JobSubmitter too

--- restartComponent.sh
+++ restartComponent.sh
@@ -2,16 +2,20 @@
 
 HOST=`hostname`
 DATENOW=`date +%s`
-LASTCHANGE=`stat -c %Y /data/srv/wmagent/current/install/wmagent/ErrorHandler/ComponentLog`
-INTERVAL=`expr $DATENOW - $LASTCHANGE`
-if (("$INTERVAL" >= 900)); then
-  OTHERS=`ps aux | grep wmcore | grep -v grep`
-  if [[ -z "$OTHERS" ]]; then
-    echo "Since the agent is not running, don't do anything ..."
-    exit 1
-  fi
+COMPONENTS="ErrorHandler JobSubmitter"
+for comp in $COMPONENTS; do
+  LASTCHANGE=`stat -c %Y /data/srv/wmagent/current/install/wmagent/$comp/ComponentLog`
+  INTERVAL=`expr $DATENOW - $LASTCHANGE`
+  if (("$INTERVAL" >= 900)); then
+    OTHERS=`ps aux | grep wmcore | grep -v grep`
+    if [[ -z "$OTHERS" ]]; then
+      echo "Since the agent is not running, don't do anything ..."
+      exit 1
+    fi
 
-  . /data/admin/wmagent/env.sh
-  /data/srv/wmagent/current/config/wmagent/manage execute-agent wmcoreD --restart --components=ErrorHandler
-  echo "cronjob in $HOST" | mail -s "ErrorHandler restarted" alan.malta@cern.ch,sryu@fnal.gov
-fi
+    . /data/admin/wmagent/env.sh
+    /data/srv/wmagent/current/config/wmagent/manage execute-agent wmcoreD --restart --components=$comp
+    echo "ComponentLog quiet for $INTERVAL secs" | mail -s "$HOST : $comp restarted" alan.malta@cern.ch,sryu@fnal.gov
+  fi
+done
+
