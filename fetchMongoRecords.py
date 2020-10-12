65efe78a8b96d1a483315358cfd9cb0888ced2f2
Alan Malta Rodrigues <alan.malta@cern.ch>
2020-10-12 10:55:43 +0200

    Fetch all docs and create a map of all tape rules

--- fetchMongoRecords.py
+++ fetchMongoRecords.py
@@ -8,7 +8,6 @@ from __future__ import print_function, division
 import sys
 import json
 import pymongo
-from pprint import pprint
 
 def main():
     myClient = pymongo.MongoClient("mongodb://localhost:8230/")
@@ -16,10 +15,12 @@ def main():
     collections = ["msOutRelValColl", "msOutNonRelValColl"]
 
     summaryByContainer = {}
-    myQuery = {'RequestType': 'Resubmission'}
     for coll in collections:
+        if coll == "msOutRelValColl":
+            print("RelVals don't run ACDC workflows, so move to the next collection")
+            continue
         myCol = myDB[coll]
-        resp = myCol.find(myQuery)
+        resp = myCol.find()
         docs = list(resp)
         print("Found {} documents in the DB collection: {}".format(len(docs), coll))
         for doc in docs:
@@ -28,7 +29,14 @@ def main():
                     summaryByContainer.setdefault(outItem['Dataset'], [])
                     summaryByContainer[outItem['Dataset']].append(dict(rse=outItem['TapeDestination'],
                                                                        ruleId=outItem['TapeRuleID']))
-        print("  and there are {} containers with wrong Tape rules.".format(len(summaryByContainer)))
+
+        print("  there are a total of {} containers with Tape rules".format(len(summaryByContainer)))
+        # now filter containers with more than one Tape rule
+        for contName in list(summaryByContainer):
+            if len(summaryByContainer[contName]) == 1:
+                print("Dropping single rule container: {}".format(contName))
+                summaryByContainer.pop(contName, None)
+        print("  where {} have multiple Tape rules (perhaps dup rules too).".format(len(summaryByContainer)))
 
         fileName = "/data/user/{}_tapeRules.json".format(coll)
         print("saving summary in file: {}\n".format(fileName))
